d0ad010ea9a4164923a4d26a5691b68d340444a3
widjaja <wwidjaja0@gmail.com>
2024-12-17 23:47:43 -0800

    stylize the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,18 +6,6 @@ import {
 } from "react-router-dom";
 import ROUTES from "./routesConfig";
 
-/**
- * A base for a festive-card that I can send to anyone in the world.
- *
- * What does it contain?
- * 1. A sticky calendar that updates as we scroll through the photos with dates.
- * 2. Photos that animate in with animated text that is stylized to the occasion.
- * 3. A main page that describes what the website is about where you can view the sylized cards for others.
- * 4. A particle system.
- * 5. A nice note section at the end.
- * 6. A copyright/crediting section at the end.
- */
-
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		ROUTES.map(({ path, element }) => (
--- frontend/src/pages/CardPage.tsx
+++ frontend/src/pages/CardPage.tsx
@@ -1,6 +1,18 @@
 import { useParams } from "react-router-dom";
 import Snowfall from "react-snowfall";
 
+/**
+ * A base for a festive-card that I can send to anyone in the world.
+ *
+ * What does it contain?
+ * 1. A sticky calendar that updates as we scroll through the photos with dates.
+ * 2. Photos that animate in with animated text that is stylized to the occasion.
+ * 3. A main page that describes what the website is about where you can view the sylized cards for others.
+ * 4. A particle system.
+ * 5. A nice note section at the end.
+ * 6. A copyright/crediting section at the end.
+ */
+
 function CardPage() {
 	const params = useParams();
 	const useSnow = true;
@@ -10,6 +22,78 @@ function CardPage() {
 	return (
 		<div>
 			{params.cardName}
+			<div>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum."
+			</div>
+			<div>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum."
+			</div>
+			<div>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum."
+			</div>
+			<div>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum."
+			</div>
+			<div>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum."
+			</div>
+			<div>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum."
+			</div>
+			<div>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum."
+			</div>
+			<div>
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum."
+			</div>
 			{useSnow && <Snowfall snowflakeCount={30} />}
 		</div>
 	);
